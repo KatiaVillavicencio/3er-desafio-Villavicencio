01d600da647c82c6c95bfcae3ec9216366889e18
Katia Villavicencio <125634854+KatiaVillavicencio@users.noreply.github.com>
2023-09-04 00:11:13 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,35 @@
+import express from "express";
+import ProductManager from "./ProductManager.js";
+
+const manager = new ProductManager("./files/products.json");
+const app = express();
+const PORT = 8080;
+
+app.get("/products", async (req, res) => {
+  const { limit } = req.query;
+  const products = await manager.getProducts();
+  if (limit) {
+    const limitedProducts = products.slice(0, limit);
+    res.json({limitedProducts });
+  } else {
+    res.json({products});
+  }
+});
+
+app.get("/products/:idProduct", async (req, res) => {
+
+  const { idProduct } = req.params;
+
+  const products = await manager.getProducts();
+
+  const productFind = products.find((p) => p.id === parseInt(idProduct));
+  if (!productFind) return res.send({ error: " No se encuentra el producto" });
+
+  console.log(productFind);
+
+  res.send({productFind});
+});
+
+app.listen(PORT, () => {
+  console.log(`server escuchando en ${PORT}`);
+});
